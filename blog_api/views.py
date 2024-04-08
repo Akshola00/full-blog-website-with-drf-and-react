1e533c132a38cea94e053b36244babb0cb7386c8
Akshola00 <akinniyishola07@gmail.com>
2024-04-08 20:43:45 +0100

    progress

---
+++ blog_api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAdminUser, DjangoModelPermissionsOrAnonReadOnly
+
+
+class Postlist(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    queryset = Post.postobjects.all() 
+    serializer_class = PostSerializer
+    pass
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pass
